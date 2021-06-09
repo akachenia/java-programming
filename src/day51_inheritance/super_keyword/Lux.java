47d61530e6c8b8acdf37e5dbeedb2ad7e9fba622
akachenia <akachenia@gmail.com>
2021-06-08 22:08:10 -0400

    object class intro

---
+++ src/day51_inheritance/super_keyword/Lux.java
@@ -0,0 +1,12 @@
+package day51_inheritance.super_keyword;
+
+public class Lux extends Lyft{
+
+    @Override
+    public double calculateRate(double miles){
+        // calculateRate of Lyft + plus 20 present
+
+        return super.calculateRate(miles) * 1.2;
+
+    }
+}
