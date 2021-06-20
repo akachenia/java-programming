9190454ef6dcb8dda2157b3747194be06cb678b6
akachenia <akachenia@gmail.com>
2021-06-20 11:52:05 -0400

    abstract class practice

---
+++ src/day55_abstraction/exercis_example/Exercise.java
@@ -0,0 +1,25 @@
+package day55_abstraction.exercis_example;
+
+public abstract class Exercise {
+    public void start(){//  non -abstract method
+        System.out.println("Warming up and starting the exercise");
+
+    }
+    /**
+    abstract method - method without body/impl, just signature
+    purpose: letting sub classes implement/override their own way
+     */
+    public abstract void perform(); // method can;t be final and abstract at the same time.
+                                     // final means not abstract
+                                    // can't be private and abstract at the same time
+
+
+    /**
+     * another abstract method that concrere sub classes will override/implement
+     * @param minutes - how long is exercise ir performed
+     * @return number of calories burned/used
+
+     */
+    public abstract int getCaloriesCount(int minutes);
+
+}
---
+++ src/practice_replit/unit4.java
@@ -0,0 +1,15 @@
+package practice_replit;
+
+public class unit4 {
+    static void run(){
+        System.out.println(1);
+    }
+    public static void main (String [] args){
+        System.out.println(2);
+    }
+
+static{
+    System.out.println(3);
+}
+    
+}
